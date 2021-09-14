145ce9c48d6f38f8024f33a5972d9269d32322dc
AsherWhi <89489616+AsherWhi@users.noreply.github.com>
2021-09-14 15:13:11 -0700

    Finished Part 2

--- Seed.java
+++ Seed.java
@@ -12,8 +12,14 @@ public class Seed extends Actor
      * Act - do whatever the Worm wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public int actN = 0;
+    
+    
     public void act()
     {
-        // Add your action code here.
+         move(10);
+         if(actN%10 == 1)
+         turn(Greenfoot.getRandomNumber(360));
+         actN++;
     }
 }
